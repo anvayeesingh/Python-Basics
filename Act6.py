30d1a4449132ce18916ee8f0af2a98ab79f6234c
anvayeesingh <anvayeesingh@gmail.com>
2026-02-23 16:38:42 +0530

    Add files via upload

---
+++ Act6.py
@@ -0,0 +1,12 @@
+
+a = 55
+print(type(a))
+
+b = 4.61
+print(type(b))
+
+c = "Codingal"
+print(type(c))
+
+d = False
+print(type(d))
---
+++ Act7.py
@@ -0,0 +1,35 @@
+name = "Riya"
+age = 10
+weight = 28.45 
+height = 147
+is_student = True
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(weight)
+print(type(weight))
+
+print(height)
+print(type(height))
+
+print(is_student)
+print(type(is_student))
+
+print("\n After typecasting")
+age = str(age)
+print(age)
+print(type(age))
+
+print("\n After typecasting")
+weight = int(weight)
+print(weight)
+print(type(weight))
+
+i = input("Enter a word ")
+revtext = i[::-1]
+
+print(revtext)

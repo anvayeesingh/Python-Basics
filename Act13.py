b757395e6ada06ddb026fa61b9b64ecbe29ce692
anvayeesingh <anvayeesingh@gmail.com>
2026-03-02 11:48:56 +0530

    Add files via upload

---
+++ Act13.py
@@ -0,0 +1,10 @@
+
+def calculate_square_root_exponent(number):
+    result = number ** 0.5
+    return result
+
+num = float(input("Enter a number: "))
+sqrt_result = calculate_square_root_exponent(num)
+print(f"The square root of {num} is: {sqrt_result}")
+
+

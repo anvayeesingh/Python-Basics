f86e5efbd073c276d163e8b2d8aeee13450fe9a2
anvayeesingh <anvayeesingh@gmail.com>
2026-03-02 11:36:19 +0530

    Add files via upload

---
+++ Act11.py
@@ -0,0 +1,11 @@
+abc = " Sabrina : 24 March"
+te = "Jennie : 16 Janary"
+ghi = " Sneh : 5 December"
+jkl = "Akshita : 3 January"
+mno = " Harsh : 22 October"
+
+print(abc)
+print(te)
+print(ghi)
+print(jkl)
+print(mno)

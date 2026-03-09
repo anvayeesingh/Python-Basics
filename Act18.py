931cc3dbe35b21392f382f5d51764abfb6fe0faa
anvayeesingh <anvayeesingh@gmail.com>
2026-03-09 16:44:02 +0530

    Add files via upload

---
+++ Act18.py
@@ -0,0 +1,47 @@
+
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the number have boolean values is True.")
+else:
+    print("Atleat one number has boolean value is False.")
+
+A  = 10
+B = -10
+C = 0
+
+if A > 0 or B > 0:
+     print("Either of the number is greater than 0.")
+else:
+    print("No number is greater than 0.")
+
+if B > 0 or C > 0:
+    print("Either of the number is greater than 0.")
+else:
+    print("No number is greater than 0.")
+
+
+x = 10
+y = 11
+z = 11
+
+print (x != y)
+
+q = "python"
+r = "coding"
+
+if q != r:
+    print(q, "and" b, "are different.")
+
+g = 4 
+h = 5
+
+if (g == 1) != (h == 5)
+    print("Hello")
+
+m = int(input("Enter a number "))
+
+if m%2 != 0:
+    print(m, "is not an even number.")

a4d827649514855826e0388ce564f4d2553b3aef
anvayeesingh <anvayeesingh@gmail.com>
2026-03-18 17:49:25 +0530

    Add files via upload

---
+++ Act23.py
@@ -0,0 +1,18 @@
+
+x = 8
+y = 0
+g = 10
+pqr = 4
+rst = 5
+
+z = (pqr +rst) ** (y/x)
+print("Value of (pqr+rst) ** (y/x) = ", z )
+
+name = "Alex"
+age = 0
+
+if name=="Alex" or  "John" and age>=2:
+    print("Welcome")
+else:
+    print("Goodbye")
+

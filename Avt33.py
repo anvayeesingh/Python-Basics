b17b6663058d7c5aff0ce2a703f679787c30391b
anvayeesingh <anvayeesingh@gmail.com>
2026-03-23 16:34:47 +0530

    Add files via upload

---
+++ Avt33.py
@@ -0,0 +1,7 @@
+
+string = (input("Please enter your string"))
+string2 = ('')
+for i in string:
+    string2 =  i + string2
+print ("\nThe orginal string was: ", string)
+print("The reversed string is: ", string2)

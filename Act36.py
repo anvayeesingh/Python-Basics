42123d59e64217d60e6d4dc09f77f71a428f9e4b
anvayeesingh <anvayeesingh@gmail.com>
2026-03-25 16:06:31 +0530

    Add files via upload

---
+++ Act36.py
@@ -0,0 +1,7 @@
+
+age = int(input("Enter your age: "))
+
+if age >= 10 and age <= 20:
+    print(f"The age {age} is between 10 and 20 years.")
+else:
+    print(f"The age {age} is NOT between 10 and 20 years.")

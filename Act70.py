396dd7dacbfe7400357beecf263e9237c221602e
anvayeesingh <anvayeesingh@gmail.com>
2026-06-03 16:40:24 +0530

    Add files via upload

---
+++ Act70.py
@@ -0,0 +1,16 @@
+tuplex = ("Tuple", False, 3.1, 1)
+print(tuplex)
+
+tuplex = (4, 6, 2, 8, 3, 1)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
+
+tuplex = (6, 7, 8, 9, 5, 4 , 3, 2, 1)
+slice = tuplex[3:7]
+print(slice)
+slice = tuplex[:6]
+print(slice)

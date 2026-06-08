31f4081dbb41e2cd346ab154e13f03034c54f48e
anvayeesingh <anvayeesingh@gmail.com>
2026-06-08 16:04:51 +0530

    Add files via upload

---
+++ Act78.py
@@ -0,0 +1,10 @@
+months = (
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+)
+
+print("Months of the year:")
+
+for month in months:
+    print(month)

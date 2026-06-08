363e6aa2da648a66b389c0e20a1a6d137000b4ac
anvayeesingh <anvayeesingh@gmail.com>
2026-06-08 16:07:10 +0530

    Add files via upload

---
+++ Act79.py
@@ -0,0 +1,10 @@
+import math
+
+angle = float(input("Enter the angle in degrees: "))
+
+
+radian = math.radians(angle)
+
+print("sin(", angle, ") =", math.sin(radian))
+print("cos(", angle, ") =", math.cos(radian))
+print("tan(", angle, ") =", math.tan(radian))

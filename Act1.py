243d15ac68029e4bfe45e34217a5ee19183340bf
anvayeesingh <anvayeesingh@gmail.com>
2026-08-17 16:42:26 +0530

    Add files via upload

--- Act1.py
+++ Act1.py
@@ -1,6 +1,3 @@
+import pygame
+import random
 
-print("Hello, My name is  Anvayee.")
-print("I am 9 years old.")
-print("I like to draw.")
-print("The result of 10+5 is ",10+5)
-print("The product of 6 and 2 is ",6*2 )
